7eec734b1d130cd855f473bc096fe07a6d037ea2
bye zshan <byezshan196@gmail.com>
2025-08-25 17:08:01 +0500

    Fixed static files and added STATIC_ROOT

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Expense-Manager.up.railway.app']
 
 
 # Application definition
